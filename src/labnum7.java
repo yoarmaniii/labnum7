d09208322dee875d500fb18bb657a685630cf095
Armani Raleigh <armanijolee@gmail.com>
2016-09-15 17:29:09 -0400

    progressing with the first question

--- src/labnum7.java
+++ src/labnum7.java
@@ -4,13 +4,31 @@ public class labnum7 {
 
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
+		String studentName;
 		//Declare userInput
 		int input;
+		String choice;
 		//Welcome Screen question
 		System.out.println("Welcome! Which Student would you like to learn more about? (Enter a number between 1-5)");
-	
+		
 		//Get userinput
-		input = userInput.nextInt();
+		input = userInput.nextInt();		
+		//Start switch somewhere
+		switch (input){
+		case 1 :
+			choice =userInput.nextLine();
+			System.out.println("This student is Armani " + "Would you like to know about Armani? (enter Hometown or Favorite food");
+				if(choice.equals("hometown")){
+					System.out.println("Armani is from Detroit, MI! Would you like to know more or nah? Yes/No?");
+					}
+						if(choice.equalsIgnoreCase("yes")){
+							System.out.println( "Enter Hometown or Favorite food");
+						}
+						else {
+							System.out.println("Do you want to continue with another?");
+							break;
+						}
+		}
 	}
 
 }
