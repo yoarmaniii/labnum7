725bbffc15d082b238adc98fefc6d2d3a3ca68ac
Armani Raleigh <armanijolee@gmail.com>
2016-09-15 16:37:54 -0400

    Beginning of Lab 7

---
+++ src/labnum7.java
@@ -0,0 +1,11 @@
+//Import your Scanner
+import java.util.Scanner;
+public class labnum7 {
+
+	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+		//Welcome Screen question
+		System.out.println("Welcome! Which Student would you like to learn more about? (Enter a number between 1-5)");
+	}
+
+}
