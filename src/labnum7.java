4ff91fd5e423190c3a9991e49947d29c3d402549
Armani Raleigh <armanijolee@gmail.com>
2016-09-15 16:46:07 -0400

    Lab7 Progress 2

--- src/labnum7.java
+++ src/labnum7.java
@@ -4,8 +4,13 @@ public class labnum7 {
 
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
+		//Declare userInput
+		int input;
 		//Welcome Screen question
 		System.out.println("Welcome! Which Student would you like to learn more about? (Enter a number between 1-5)");
+	
+		//Get userinput
+		input = userInput.nextInt();
 	}
 
 }
