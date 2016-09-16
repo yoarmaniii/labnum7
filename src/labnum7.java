dc56f380c49ad85fe861131c70bfce8f071a189e
Armani Raleigh <armanijolee@gmail.com>
2016-09-16 13:01:56 -0400

    Its workinggit status :)

--- src/labnum7.java
+++ src/labnum7.java
@@ -4,73 +4,58 @@ public class labnum7 {
 
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
-		String studentName;
+		String userCont = "yes";
+		boolean cont = true;
+		
 		
 		//Declare userInput
 		int input;
 		String choice;
 		
 		//Welcome Screen question
-		System.out.println("Welcome! Which Student would you like to learn more about? (Enter a number between 1-5)");
 		
-		//Get userinput
-		input = userInput.nextInt();	
-		
+		//Get userinput		
 		//Start switch here
-		switch (input){
+		//insert a While loop 
+		while(userCont.equals("yes"))	
+		{	
+			System.out.println("Welcome! Which Student would you like to learn more about?"+ "(Enter a number between 1-5)");
+
+			input = userInput.nextInt();
+			switch (input){
 		case 1 :
 			choice =userInput.nextLine();
-			System.out.println("This student is Armani! " + "What would you like to know about Armani? (enter Hometown or Favorite food)");
+			System.out.println("This student is Armani! " + "What would you like to know where Armani is from? (enter Yes or No)");
 				choice = userInput.nextLine();
-				
-				//userinput of choices for hometown or favorite food
-			if(choice.equalsIgnoreCase("hometown")){
-					System.out.println("Armani is from Detroit, MI! Would you like to know more or nah? Enter - Yes/No?");
+		
+				//user Input is yes
+			if(choice.equalsIgnoreCase("yes")){
+					System.out.println("Armani is from Detroit, MI! Would you like to know more? Enter - Yes/No?");
 					choice = userInput.nextLine();
 					
-					if (choice.equalsIgnoreCase("yes")){
-						System.out.println( "Enter Hometown or Favorite food");
-						choice = userInput.nextLine();
-					break;
-						}//end of While statement
-					} //end of if statement
-			else if(choice.equalsIgnoreCase("no")){
-				System.out.println("Do you want to continue with another student?" + "Enter Yes/No");
-				choice = userInput.nextLine();
+					if(choice.equalsIgnoreCase("yes")){
+						System.out.println("Armani's favorite food is Salmon!");
+						System.out.println("Continue with another student? Enter Yes/No");
+						userCont = userInput.nextLine();
+					}
+					//user Input is no
+			else if (choice.equalsIgnoreCase("no")){
+						System.out.println("Continue with another student?" + "Yes/No");
+						userCont = userInput.nextLine();
+						
+						
+					}
+			
+			}
 				
+					break;
+			}//s
+			
+
+		}//w
+		System.out.println("goodbye");
 		}
-				if(choice.equalsIgnoreCase("no")) {
-					System.out.println("Until next time");
-					
-				}
-					while (choice.equalsIgnoreCase("Favorite Food")){
-						System.out.println(" Armani's favorite food is Salmon! YUMMMYYY!!!" + "Would you like to know more about Armani?" + "Enter Yes/No");
-						choice = userInput.nextLine();
-						while (choice.equalsIgnoreCase("yes")){
-							System.out.println( "Enter Hometown or Favorite food");
-							choice = userInput.nextLine();
-						break;
-							}
-						}//end of "Else If" statement
-						
-						while (choice.equalsIgnoreCase("yes")){
-							System.out.println( "Enter Hometown or Favorite food");
-							choice = userInput.nextLine();
-							break;
-							}//end of "If" statement
-						
 
-						
-						 {
-							if(choice.equalsIgnoreCase("no"))
-							System.out.println("Do you want to continue with another student?");
-							choice = userInput.nextLine();
-							break;
-						}//end of last "else" statement
-							
-						}//end of switch method
-		
 	
-		}//Main method ends
-}
-
+	}
+	
